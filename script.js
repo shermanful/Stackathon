0b195d785aafa63cafef309f50f780af9f083116
Jill Sherman <jillsherman@MacBook-Pro.local>
2022-01-23 13:44:44 -0500

    phase 2

--- script.js
+++ script.js
@@ -3,25 +3,7 @@ const try_me = document.querySelector('#try_me');
 let imageFile;
 let uploaded_image;
 let net;
-
 var image_to_test;
-
-// = new Image(); // Image constructor
-// image_to_test.src = imageFile.name;
-// image_to_test.alt = 'alt';
-// document.body.appendChild(image_to_test);
-
-// function thisDoesNothing() {
-//   //{imageFile} = returns object
-//   ////// imageFile: File {name: 'defaultImage.jpg', lastModified: 1642821742544, lastModifiedDate: Fri Jan 21 2022 22:22:22 GMT-0500 (Eastern Standard Time), webkitRelativePath: '', size: 958070, …}[[Prototype]]: Object
-//   //imageFile.name = returns string
-//   ///////defaultImage.jpg
-//   //imageFile = returns file
-//   ////// File {name: 'defaultImage.jpg', lastModified: 1642821742544, lastModifiedDate: Fri Jan 21 2022 22:22:22 GMT-0500 (Eastern Standard Time), webkitRelativePath: '', size: 958070, …}
-//   //'defaultImage.jpg' - returns string
-
-//   return true;
-// }
 
 async function app() {
   console.log('Loading mobilenet..');
@@ -29,9 +11,8 @@ async function app() {
   // Load the model.
   net = await mobilenet.load();
   console.log('Successfully loaded model');
-  image_to_test = createImage();
   //////// ###############################################
-  const imgEl = image_to_test; //// what is the image? How do I enter it???
+  const imgEl = uploaded_image; //// what is the image? How do I enter it???
   console.log('is this an image?', imgEl);
 
   // Make a prediction through the model on our image.
@@ -53,12 +34,6 @@ upload_an_image.addEventListener('change', function () {
   reader.readAsDataURL(this.files[0]);
 });
 
-async function createImage() {
-  await upload_an_image();
-  image_to_test = new Image(); // Image constructor
-  image_to_test.src = imageFile.name;
-}
-
 try_me.addEventListener('click', function () {
   if (uploaded_image) {
     console.log('Try Me Clicked!');
@@ -67,3 +42,18 @@ try_me.addEventListener('click', function () {
     console.log('Nothing to test!');
   }
 });
+
+//////// with their html this is example script from tensorflow
+// async function app() {
+//   console.log('Loading mobilenet..');
+
+//   // Load the model.
+//   net = await mobilenet.load();
+//   console.log('Successfully loaded model');
+//   //////// ###############################################
+//   const imgEl = document.getElementById('img');
+//   const result = await net.classify(imgEl);
+//   console.log(result);
+// }
+
+// app();
